c01772f6e3314791d688243e280c345e81151ab0
tieulac <nlac283@gmail.com>
2024-09-19 13:41:06 +0700

    update form

--- Lab2_3/Form1.Designer.cs
+++ Lab2_3/Form1.Designer.cs
@@ -28,12 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(41, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 23);
+            label1.TabIndex = 0;
+            label1.Text = "Số a";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(41, 106);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 23);
+            label2.TabIndex = 1;
+            label2.Text = "Số b";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(41, 165);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 23);
+            label3.TabIndex = 2;
+            label3.Text = "Kết quả";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(154, 39);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(232, 30);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(154, 103);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(232, 30);
+            textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(154, 162);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(232, 30);
+            textBox3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(292, 229);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 4;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(430, 284);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Tính toán";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
     }
 }
--- Lab2_3/Form1.cs
+++ Lab2_3/Form1.cs
@@ -6,5 +6,10 @@ namespace Lab2_3
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
