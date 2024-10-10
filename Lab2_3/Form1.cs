940f516079ce988bea1107abfba218d907273e62
Mainhi110204 <hochoc150@gmail.com>
2024-10-10 13:40:04 +0700

    Them button nhan va xu ly button nhan

--- Lab2_3/Form1.cs
+++ Lab2_3/Form1.cs
@@ -30,5 +30,14 @@ namespace Lab2_3
             a - b;
             txtKetQua.Text = c.ToString();
         }
+
+        private void btnNhan_Click(object sender, EventArgs e)
+        {
+            double a =
+            double.Parse(txtA.Text); double b =
+            double.Parse(txtB.Text); double c =
+            a * b;
+            txtKetQua.Text = c.ToString();
+        }
     }
 }
